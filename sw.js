43e29ea112c1286478512c501a2429c3268d5d35
rgarciaa78-svg <rgarciaa78@gmail.com>
2026-08-22 19:59:22 -0500

    chore: sw v14 cache bust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v13';
+const VERSION = 'v14';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
