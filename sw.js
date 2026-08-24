3964a44352e16d96e918e26dfc8f597c3e96c806
rgarciaa78-svg <rgarciaa78@gmail.com>
2026-08-24 17:59:40 -0500

    chore: bump SW to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v14';
+const VERSION = 'v15';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
